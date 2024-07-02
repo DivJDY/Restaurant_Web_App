039e2b19d34c9848911875a5ebcf0b3aa223a26a
divyah.hs@yugasys.com <divya.hs@gmail.com>
2024-07-02 14:33:05 +0530

    Restaurant Web App with CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  Suspense,
+  // useEffect, useState
+} from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import AddRestaurant from "./components/AddRestaurant";
+import EditRestaurant from "./components/EditRestaurant";
+import RestaurantList from "./components/RestaurantList";
 
-function App() {
+const App: React.FC = () => {
+  // const [height, setHeight] = useState(window.innerHeight);
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setHeight(window.innerHeight);
+  //   };
+  //   window.addEventListener("resize", handleResize);
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.StrictMode>
+      <Suspense
+        fallback={
+          <div
+            style={{
+              color: "#000",
+              fontWeight: 600,
+              textAlign: "center",
+              marginTop: "20px",
+            }}
+          >
+            Loading...
+          </div>
+        }
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<RestaurantList />} />
+            <Route path="/add" element={<AddRestaurant />} />
+            <Route path="/edit/:id" element={<EditRestaurant />} />
+            {/* Default Route */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </React.StrictMode>
   );
-}
+};
 
 export default App;
